297137bc82f834c1008d36920c0e12d6d85e1457
wangxiaoqiang <aftercoding.com>
2020-03-31 15:13:33 +0800

    add class:LayerData

--- src/core/main.py
+++ src/core/main.py
@@ -3,6 +3,19 @@
 import sys
 from scapy.all import sr1,IP,ICMP
 
-p=sr1(IP(dst=sys.argv[1])/ICMP())
+class LayerData(object):
+	"""docstring for ClassName"""
+	def __init__(self):
+		super(ClassName, self).__init__()
+		self.name = None
+		self.attribute = None
+	
+
+
+protocol1 = "IP"
+ipdst = "192.168.0.1"
+protocol2 = "ICMP"
+
+p=sr1(eval(protocol1)(dst= ipdst)/eval(protocol2)())
 if p:
     p.show()
