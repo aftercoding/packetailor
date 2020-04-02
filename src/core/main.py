dc41e2b1fb4b7429b9cd8d65df1cee888645bbc9
wangxiaoqiang <aftercoding.com>
2020-04-02 16:23:47 +0800

    update main

--- src/core/main.py
+++ src/core/main.py
@@ -1,32 +1,14 @@
 #! /usr/bin/env python
 
 import sys
-from scapy.all import sr1,IP,ICMP
+from packetSuiter import *
 
-class LayerData(object):
-	"""docstring for ClassName"""
-	def __init__(self,name):
-		#super(ClassName, self).__init__()
-		self.name = name
-		self.attribute = None
-	
-	def setAttribute(self):
-		self.attribute = {'dst': '192.168.0.1'}
+#cmdStr = layerData1.name + '(' + 'dst' + '=' + ' \'192.168.0.1\'' + ')' +'/' + layerData2.name +'(' +''+ ')'
 
+#print (cmdStr)
 
-layerData1 = LayerData('IP')
-layerData1.setAttribute()
-layerData2 = LayerData('ICMP')
+p = PacketSuiter()
 
-cmdStr = layerData1.name + '(' + 'dst' + '=' + ' \'192.168.0.1\'' + ')' +'/' + layerData2.name +'(' +''+ ')'
+p.loadSuite()
+p.run()
 
-print (cmdStr)
-
-
-p = sr1(eval(cmdStr))
-if p:
-	p.show()
-
-#p=sr1(eval(protocol1)(dst= ipdst)/eval(protocol2)())
-#if p:
-#    p.show()
