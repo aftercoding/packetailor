a84198b780c41709590d454dfcacedb7f24e380f
wangxiaoqiang <aftercoding.com>
2020-04-08 22:01:43 +0800

    add jason

--- src/core/suiteParser.py
+++ src/core/suiteParser.py
@@ -1,5 +1,7 @@
-# import sys
+import sys
+import os.path
 
+import json 
 		
 
 class LayerData(object):
@@ -16,6 +18,31 @@ class LayerData(object):
 	def getAttribute(self):
 		return self.attribute 
 
+
+# data = {
+#     'protocol-name' : 'IP',
+#    'dst': '192.168.0.3',
+# }
+
+# with open('data.json', 'w') as f:
+#     json.dump(data, f)
+
+with open('D:\\packetailor\\samples\\ip-icmp.json', 'r') as f:
+# with open('data.json', 'r') as f:
+    data = json.load(f)
+print (data)
+
+# with open ('D:\\packetailor\\samples\\ip-icmp.json',"r") as f:
+# 	temp = json.loads(f.read())
+# 	print(temp)
+
+
+
+# f = open('D:\\packetailor\\samples\\ip-icmp.json',"r")
+# # print(f.readlines())
+
+# print (js.load(f))
+# f.close()
 # layerData1 = LayerData('IP')
 # layerData1.setAttribute()
 # layerData2 = LayerData('ICMP')
